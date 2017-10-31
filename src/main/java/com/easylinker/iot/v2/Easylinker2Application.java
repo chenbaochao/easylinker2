a82babdb1ea4c40349e8daa794c494ec6b7ed1ed
Administrator <751957846@qq.com>
2017-10-31 15:06:04 +0800

    环境搭建

--- src/main/java/com/easylinker/iot/v2/Easylinker2Application.java
+++ src/main/java/com/easylinker/iot/v2/Easylinker2Application.java
@@ -2,11 +2,23 @@ package com.easylinker.iot.v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+/**
+ * 启动入口
+ */
+@EnableWebSecurity
+@EnableScheduling
+@EnableWebMvc
+@ServletComponentScan
 @SpringBootApplication
+
 public class Easylinker2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Easylinker2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Easylinker2Application.class, args);
+    }
 }
