8a0861f9ea828e705a661f1acbc41fa3015ab04c
Administrator <751957846@qq.com>
2017-10-31 13:40:22 +0800

    新版本的EasyLinker

---
+++ src/main/java/com/easylinker/iot/v2/Easylinker2Application.java
@@ -0,0 +1,12 @@
+package com.easylinker.iot.v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easylinker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easylinker2Application.class, args);
+	}
+}
---
+++ src/test/java/com/easylinker/iot/v2/Easylinker2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.easylinker.iot.v2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Easylinker2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
