e0acbc8cd628dc72b745134d23b5db2f8115937a
Administrator <751957846@qq.com>
2018-01-06 20:53:11 +0800

    跨域配置

---
+++ src/main/java/com/easylinker/iot/v2/configure/boot/JsCrossRequestConfig.java
@@ -0,0 +1,29 @@
+package com.easylinker.iot.v2.configure.boot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created by wwhai on 2018/1/6.
+ * 跨域请求配置
+ */
+@Configuration
+public class JsCrossRequestConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1
+        corsConfiguration.addAllowedHeader("*"); // 2
+        corsConfiguration.addAllowedMethod("*"); // 3
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+}
